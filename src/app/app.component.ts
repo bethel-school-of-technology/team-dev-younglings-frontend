b44ab2a374d55f7fd5a9ed133e91751370b2ea44
Spikeist <parkerdaniels2805@gmail.com>
2024-03-27 18:30:30 -0500

    created a way to logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-dev-younglings-frontend';
+
+  constructor(public userService: UserService) {}
 }
